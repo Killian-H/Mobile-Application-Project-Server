e40be213444facc7f4ec21546dde84a137f82858
VictorZhong4475 <zhengz42@uw.edu>
2021-11-14 21:17:20 -0800

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -1,33 +1,42 @@
 //express is the framework we're going to use to handle requests
 const express = require('express')
 
-//retrieve the router object from express
-var router = express.Router()
+//Access the connection to Heroku Database
+const pool = require('../utilities').pool
 
-/**
- * @api {get} /hello Request a Hello World message
- * @apiName GetHello
- * @apiGroup Hello
- *
- * @apiSuccess {String} message Hello World message
- */
-router.get("/", (request, response) => {
-    response.send({
-        message: "Hello, you sent a GET request"
-    })
+const validation = require('../utilities').validation
+let isStringProvided = validation.isStringProvided
+
+const generateHash = require('../utilities').generateHash
+
+const router = express.Router()
+
+
+router.get('/' , (request, response) => {
+    const theQuery = "SELECT * FROM Members WHERE Memberid = $1"
+    const values = 6
+    pool.query(theQuery, values)
+        .then(result => { 
+
+            response.send({
+
+                memberid:values,
+                rowCounts:result.rowCount,
+                rows:result.rows
+
+
+            })
+
+            
+            
+        })
+        .catch((err) => {
+            //log the error
+            
+            response.status(400).send({
+                message: "SQL Error"
+            })
+        })
 })
 
-/**
- * @api {post} /hello Request a Hello World message
- * @apiName PostHello
- * @apiGroup Hello
- * 
- * @apiSuccess {String} message Hello World message
- */ 
-router.post("/", (request, response) => {
-    response.send({
-        message: "Hello, you sent a POST request"
-    })
-})
-// "return" the router
 module.exports = router
