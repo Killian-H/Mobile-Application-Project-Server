2f9889f3ee85226f88eaca465b4bfe8d1a8fd73e
VictorZhong4475 <zhengz42@uw.edu>
2021-11-14 20:56:33 -0800

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -1,51 +1,21 @@
+//express is the framework we're going to use to handle requests
 const express = require('express')
 
+//retrieve the router object from express
+var router = express.Router()
 
-//Access the connection to Heroku Database
-const pool = require('../utilities/exports').pool
-
-const router = express.Router()
-
-const validation = require('../utilities').validation
-let isStringProvided = validation.isStringProvided
-
-
-router.get("/", (request, response,next) => {
-    next()
-    // if(!request.body.token){
-
-    //     response.status(400).send({
-            
-    //         message:"Missing required information"
-
-    //     })
-    // }
-    // else{
-    //     next()
-    // }
-
-},(request,response)=>{s
-
-    //let memberid = request.decoded.memberid
-    let memberid = 6
-    let query = `SELECT * FROM contacts WHERE contactid = $1`
-        pool.query(query, memberid)
-            .then(result => {
-                response.send({
-                    memberid: memberid,
-                    rowCount : result.rowCount,
-                    rows: result.rows
-                })
-            }).catch(err => {
-                response.status(400).send({
-                    message: "SQL Error",
-                    error: err
-                })
-            })
-    
-
-
-});
+/**
+ * @api {get} /hello Request a Hello World message
+ * @apiName GetHello
+ * @apiGroup Hello
+ *
+ * @apiSuccess {String} message Hello World message
+ */
+router.get("/", (request, response) => {
+    response.send({
+        message: "Hello, you sent a GET request"
+    })
+})
 
 /**
  * @api {post} /hello Request a Hello World message
