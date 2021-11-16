4d5a94f1a59fb2687f8b1814526e29041b254add
VictorZhong4475 <zhengz42@uw.edu>
2021-11-15 21:30:43 -0800

    contact endpoints completed
    PUT,POST,DELETE completed

--- routes/contacts.js
+++ routes/contacts.js
@@ -68,17 +68,259 @@ router.get('/' , (request, response,next) => {
 
 
 
-router.post('/',(request,response,next)=>{
 
+
+router.post("/",(request,response,next)=>{
+    
+    if(!request.decoded.memberid || !request.body.memberid_b){
+        response.status(400).send({
+
+           message: "Missing reuqired information"
+        })
+        
+    } else if(isNaN(request.body.memberid_b)){
+       
+        response.status(400).send({
+
+            message: "Malformed parameter. memberid_b must be a number"
+
+        })
+
+
+    } else {
+        next()
+    }
+
+},(request,response,next)=>{
+
+
+    let theQuery = "SELECT * FROM Members WHERE Memberid = $1"
+    let memberid_b = [request.body.memberid_b]
+
+    pool.query(theQuery,memberid_b)
+    .then(result=>{
+        
+
+
+        if(result.rowCount == 0){
+            response.status(404).send({
+
+                message:"memberid_b is not found"
+            })
+
+        } else{
+
+            next()
+        }
+
+
+    }).catch(error=>{
+
+        response.status(400).send({
+            message: "SQL Error on memberid_b check",
+            error: error
+
+        })
+
+    })
+
+
+},(request,response,next)=>{
+
+
+
+    let theQuery = "SELECT * FROM Contacts WHERE Memberid_a = $1 AND Memberid_b = $2"
+    let values = [request.decoded.memberid,request.body.memberid_b]
+
+    pool.query(theQuery,values)
+    .then(result=>{
+
+        if(result.rowCount == 1){
+
+            response.status(404).send({
+
+                message:"Memberid_b already existed in the contact"
+                
+            })
+
+        } else {
+
+            next()
+        }
+
+
+
+    }).catch(error=>{
+
+        response.status(400).send({
+
+            message:"SQL error occured at check memberid_b in contact",
+            error:error
+        })
+
+    })
     
 
+},(request,response)=>{
+
+    const verified = 0;
+    let values = [request.decoded.memberid,request.body.memberid_b,verified]
+    let theQuery = "INSERT INTO Contacts(Memberid_a,Memberid_b,Verified) VALUES($1,$2,$3) RETURNING memberid_a AS memberid,memberid_b,verified"
+    pool.query(theQuery,values)
+    .then(result=>{
+
+        if(result.rowCount == 1){
+
+            response.send({
+                
+                success:true,
+                row:result.rows[0]
+
+
+            })
+            
+
+        } else {
+
+            response.status(400).send({
+                message:"Insert failed"
+            })
+        }
+
+
+    }
+        
+        ).catch(error=>{
+
+            response.status(400).send({
+
+                message:"SQL Error on Insertion",
+                error:error
+            })
+        }
+        )
+    
+    
+
+})
+
+
+router.delete('/',(request,response,next)=>{
+
+    if(!request.decoded.memberid || !request.body.memberid_b){
+
+        response.status(400).send({
+
+            messgae:"Missing Required Information"
+
+        }) 
+
+    }else if (isNaN(request.body.memberid_b)){
+
+
+        response.status(400).send({
+
+            message:"Misformed parameter,Memberid_b must be a number"
+        })
+
+    } else {
+        next()
+
+
+    }
 
 
 
+},(request,response,next)=>{
+    
+    let values = [request.body.memberid_b]
+    let theQuery = "SELECT * FROM Members WHERE Memberid = $1"
+    pool.query(theQuery,values)
+    .then(result=>{
+
+        if(result.rowCount == 0 ){  
+            
+            response.status(404).send({
+
+                message:"Memberid_b is not existed"
+
+                })
+
+
+        } else {
+            next()
+        }
+
+
+    }).catch(error=>{
+
+        response.status(400).send({
+            messgae:"SQL Error occured at find memberid_b in the Members",
+            error:error
+
+        })
+    })
+
+},(request,response,next)=>{
+
+    let values = [request.decoded.memberid,request.body.memberid_b]
+    let theQuery = "SELECT * FROM Contacts WHERE Memberid_a = $1 AND Memberid_b = $2"
+    pool.query(theQuery,values)
+    .then(result=>{
+
+        if(result.rowCount == 0 ){
+
+            response.status(404).send({
+
+                message : "memeberid_b is not found in the contact"
+
+            })
 
 
 
+        }else{
 
+            next()
+
+        }
+
+
+    }).catch(error=>{
+
+        response.status(400).send({
+
+            message:"SQL error occured at finding memberid_in the contact",
+            error:error
+        })
+
+    })
+
+},(request,response)=>{
+
+    let values = [request.decoded.memberid,request.body.memberid_b]
+
+    let theQuery = "DELETE FROM contacts WHERE memberid_a = $1 AND memberid_b = $2 RETURNING *"
+    pool.query(theQuery,values)
+    .then(result=>{
+
+        response.send({
+
+            success:true,
+            DeletedrowNum:result.rows[0]
+
+        })
+        
+
+    }).catch(error=>{
+
+        response.status(400).send({
+
+            message:"DELETE FAILED",
+            error:error
+
+        })
+
+    })
 
 
 })
